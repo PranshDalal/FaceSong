8a8a3d2e39df9d2fe849d5cc0db5825740657c3d
PranshDalal <121412920+PranshDalal@users.noreply.github.com>
2023-10-26 18:34:57 -0700

    Add files via upload

---
+++ Server/main.py
@@ -0,0 +1,58 @@
+# backend/app.py
+from flask import Flask, request, jsonify
+import cv2
+from keras.models import model_from_json
+import numpy as np
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+# Load the model and define labels
+json_file = open("facialemotionmodel.json", "r")
+model_json = json_file.read()
+json_file.close()
+model = model_from_json(model_json)
+model.load_weights("facialemotionmodel.h5")
+
+labels = {0: 'angry', 1: 'disgust', 2: 'fear',
+          3: 'happy', 4: 'neutral', 5: 'sad', 6: 'surprise'}
+
+# Load the face cascade classifier
+haar_file = cv2.data.haarcascades + 'haarcascade_frontalface_default.xml'
+face_cascade = cv2.CascadeClassifier(haar_file)
+
+# Define a function to extract features from an image
+
+
+def extract_features(image):
+    feature = np.array(image)
+    feature = feature.reshape(1, 48, 48, 1)
+    return feature / 255.0
+
+# Route for predicting emotion
+
+
+@app.route('/predict_emotion', methods=['POST'])
+def predict_emotion():
+    image = request.files['image']
+    image = cv2.imdecode(np.fromstring(
+        image.read(), np.uint8), cv2.IMREAD_COLOR)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(image, 1.3, 5)
+
+    try:
+        for (p, q, r, s) in faces:
+            image = gray[q:q+s, p:p+r]
+            image = cv2.resize(image, (48, 48))
+            img = extract_features(image)
+            pred = model.predict(img)
+            prediction_label = labels[pred.argmax()]
+            response = {"emotion": prediction_label}
+            return jsonify(response)
+    except cv2.error:
+        pass
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
