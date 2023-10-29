839f7e11b359adf463c80f49abc286717c678306
PranshDalal <121412920+PranshDalal@users.noreply.github.com>
2023-10-28 18:08:21 -0700

    Add files via upload

--- client/src/components/EmotionVideo.js
+++ client/src/components/EmotionVideo.js
@@ -1,1 +1,34 @@
 
+import React from 'react';
+import YouTube from 'react-youtube';
+import './EmotionVideo.css';
+
+const EmotionVideo = ({ emotion }) => {
+  const videoOptions = {
+    width: 640,
+    height: 360,
+  };
+
+  const videoId = getVideoIdForEmotion(emotion);
+
+  return (
+    <div>
+      <YouTube videoId={videoId} opts={videoOptions} />
+    </div>
+  );
+};
+
+function getVideoIdForEmotion(emotion) {
+  switch (emotion) {
+    case 'happy':
+      return '1N9EFGj_x0Y';
+    case 'sad':
+      return 'xicvuiThi4Y';
+    case 'angry':
+      return 'Vbu_K41efvY';
+    default:
+      return 'Fy2wwyZMBEQ'; 
+  }
+}
+
+export default EmotionVideo;
